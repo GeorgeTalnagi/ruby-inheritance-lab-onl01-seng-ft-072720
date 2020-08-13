5c054f1c367e27eb10a0ea91234ed9c8a54acc57
George Talnagi <gtalnagi@yahoo.com>
2020-08-13 01:39:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require 'pry'
 
+class Student < User 
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end 
+  
+  def learn 
+   @knowledge << :KNOWLEDGE
+  end 
+  
+  def knowledge
+    @knowledge.student
+  end 
+  
+  
+  
 end
